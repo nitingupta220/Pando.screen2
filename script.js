5b0cc172c22e023bb09e8bff69214b318514295b
nitingupta220 <nitin16@navgurukul.org>
2017-08-06 13:47:32 +0530

    add all the files

---
+++ script.js
@@ -0,0 +1,82 @@
+// 1st Input Box
+document.getElementById("input").value = getSaveValue("input");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSaveValue(v) {
+    if (localStorage.getItem(v) === null) {
+        return 8285698087;
+    }
+    return localStorage.getItem(v);
+}
+
+// 2nd Input box
+document.getElementById("input2").value = getSaveValue("input2");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSaveValue(v) {
+    if (localStorage.getItem(v) === null) {
+        return 8285698087;
+    }
+    return localStorage.getItem(v);
+}
+
+
+// 3rd Input Box 
+document.getElementById("input3").value = getSaveValue("input3");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSaveValue(v) {
+    if (localStorage.getItem(v) === null) {
+        return 8285698087;
+    }
+    return localStorage.getItem(v);
+}
+
+// 4th Input Box
+document.getElementById("input4").value = getSaveValue("input4");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+
+function getSaveValue(v) {
+    if (localStorage.getItem(v) === null) {
+        return 8285698087;
+    }
+    return localStorage.getItem(v);
+}
+
+
+// 5th Input Box
+document.getElementById("input5").value = getSaveValue("input5");
+
+function saveValue(e) {
+    var id = e.id;
+    var val = e.value;
+    localStorage.setItem(id, val);
+}
+
+function getSaveValue(v) {
+    if (localStorage.getItem(v) === null) {
+        return 8285698087;
+    }
+    return localStorage.getItem(v);
+}
